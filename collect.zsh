5fad5861738e0256db67d24132138727b4c5426d
lyc <liuyichen626@gmail.com>
2025-05-19 12:55:45 +0800

    collect jitter data

---
+++ collect.zsh
@@ -0,0 +1,6 @@
+cp exp6_qos_queue/build/jitter6_delay.csv data/
+cp exp6_qos_queue/build/jitter6_freq.csv data/
+cp exp7_simple_queue_qos/build/jitter7_delay.csv data/
+cp exp7_simple_queue_qos/build/jitter7_freq.csv data/
+cp exp8_single_executor_qos/build/jitter8_delay.csv data/
+cp exp8_single_executor_qos/build/jitter8_freq.csv data/
