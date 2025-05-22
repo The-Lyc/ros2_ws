e9ab1dbc2e0e52af525ce7423851551d27f1400b
lyc <liuyichen626@gmail.com>
2025-05-22 23:22:58 +0800

    before and after jitter : sep data, eva

--- data/seperate_delay.py
+++ data/seperate_delay.py
@@ -2,24 +2,24 @@ import csv
 from collections import defaultdict
 
 # 分链保存数据
-chain_data = defaultdict(lambda: {"RecordTime": [], "Delay": []})
+chain_data = defaultdict(lambda: {"RecordTime": [], "Frequency": []})
 
 # 读取原始数据
-with open('jitter8_delay.csv', 'r') as f:
+with open('jitter9_freq.csv', 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         chain = row['Chain']
         chain_data[chain]["RecordTime"].append(row['RecordTime'])
-        chain_data[chain]["Delay"].append(row['Delay'])
+        chain_data[chain]["Frequency"].append(row['Frequency'])
 
 # 写入新格式的 CSV
-with open('jitter8_delay' + '_sep.csv', 'w', newline='') as f:
+with open('jitter9_freq' + '_sep.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
     for chain in sorted(chain_data.keys()):  # 按顺序写入
         writer.writerow([chain])
         writer.writerow(["RecordTime"])
         writer.writerows([[t] for t in chain_data[chain]["RecordTime"]])
-        writer.writerow(["Delay"])
-        writer.writerows([[d] for d in chain_data[chain]["Delay"]])
+        writer.writerow(["Frequency"])
+        writer.writerows([[d] for d in chain_data[chain]["Frequency"]])
 
